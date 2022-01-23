eafd87ff7f1aa0e4a1a60dba783a92bcd5653dce
yasincelebi <yasincelebix@gmail.com>
2022-01-24 02:54:39 +0300

    add loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledLoading } from "../styles/StyledTakeAction";
+
+const Loading = () => {
+  return (
+    <StyledLoading>
+      <div className="circle"></div>
+    </StyledLoading>
+  );
+};
+
+export default Loading;
