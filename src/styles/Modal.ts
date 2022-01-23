caf7a1e3d3f0fda915e28ad4c8adb0895b9731f5
yasincelebi <yasincelebix@gmail.com>
2022-01-24 02:58:31 +0300

    add z-index to modal

---
+++ src/styles/Modal.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { primaryColor } from "./variables";
+
+export const StyledModal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 100;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+`;
